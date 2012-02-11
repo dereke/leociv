4c01382184810940fdd293ca6606de4965af7142
Derek Ekins <derek@spathi.com>
2012-02-11 10:35:15 +0000

    added production and trade to city

--- lib/city.rb
+++ lib/city.rb
@@ -1,11 +1,14 @@
 class Resources
-  attr_accessor :food
+  attr_accessor :food, :gold, :production, :trade
 end
 
 class City
   def turn
     resources = Resources.new
     resources.food = 1
+    resources.gold = 2
+    resources.production = 3
+    resources.trade = 4
     resources
   end
 end
--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -1,13 +1,25 @@
 require_relative '../lib/city'
 
 describe City do
-  it "gets some food each turn" do
-    london = City.new
-    resources = london.turn
-    resources.food.should == 1
-  end
+  context "turn" do
+    before do
+      london = City.new
+      @resources = london.turn
+    end
+    it "gets some food each turn" do
+      @resources.food.should == 1
+    end
 
-  it "gets some gold each turn"
-  it "gets some production each turn"
-  it "gets some trade each turn"
+    it "gets some gold each turn" do
+      @resources.gold.should == 2
+    end
+
+    it "gets some production each turn" do
+      @resources.production.should == 3
+    end
+
+    it "gets some trade each turn" do
+      @resources.trade.should == 4
+    end
+  end
 end
