f74ce4958b4dd192d83b0c9aa0645f7cb9b49fb0
Derek Ekins <derek@spathi.com>
2012-02-11 11:17:31 +0000

    added resources to city

--- lib/city.rb
+++ lib/city.rb
@@ -1,14 +1,30 @@
 class Resources
   attr_accessor :food, :gold, :production, :trade
+  def initialize
+    @gold = 0
+    @food = 0
+    @production = 0
+    @trade = 0
+  end
 end
 
 class City
+  attr_reader :resources
+
+  def initialize
+    @resources = []
+  end
+  
   def turn
-    resources = Resources.new
-    resources.food = 1
-    resources.gold = 2
-    resources.production = 3
-    resources.trade = 4
-    resources
+    resource = Resources.new
+
+    resources.each do |city_resource|
+      resource.gold += city_resource.gold if city_resource.respond_to? :gold
+      resource.food += city_resource.food if city_resource.respond_to? :food
+      resource.production += city_resource.production if city_resource.respond_to? :production
+      resource.trade += city_resource.trade if city_resource.respond_to? :trade
+    end
+
+    resource
   end
 end
--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -3,23 +3,32 @@ require_relative '../lib/city'
 describe City do
   context "turn" do
     before do
+      mountain = mock('Mountain')
+      mountain.stub(:food).and_return(1)
+      mountain.stub(:gold).and_return(2)
+      mountain.stub(:production).and_return(3)
+      mountain.stub(:trade).and_return(4)
+
       london = City.new
+      london.resources << mountain
+      london.resources << mountain
+
       @resources = london.turn
     end
     it "gets some food each turn" do
-      @resources.food.should == 1
+      @resources.food.should == 2
     end
 
     it "gets some gold each turn" do
-      @resources.gold.should == 2
+      @resources.gold.should == 4
     end
 
     it "gets some production each turn" do
-      @resources.production.should == 3
+      @resources.production.should == 6
     end
 
     it "gets some trade each turn" do
-      @resources.trade.should == 4
+      @resources.trade.should == 8
     end
   end
 end
